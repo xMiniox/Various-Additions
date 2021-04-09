505a62e917dfb575b30b5aca23668c2ec9a7eb6a
xMiniox <wikto@DESKTOP-EAM10TR>
2021-04-09 12:30:10 +0200

    Quicksave - Deleted GUI + Added lamp

---
+++ src/main/java/net/mcreator/varioustextures/block/MagmaLampBlock.java
@@ -0,0 +1,67 @@
+
+package net.mcreator.varioustextures.block;
+
+import net.minecraftforge.registries.ObjectHolder;
+import net.minecraftforge.fml.event.lifecycle.FMLClientSetupEvent;
+import net.minecraftforge.api.distmarker.OnlyIn;
+import net.minecraftforge.api.distmarker.Dist;
+
+import net.minecraft.world.IBlockReader;
+import net.minecraft.util.math.BlockPos;
+import net.minecraft.loot.LootContext;
+import net.minecraft.item.ItemStack;
+import net.minecraft.item.Item;
+import net.minecraft.item.BlockItem;
+import net.minecraft.client.renderer.RenderTypeLookup;
+import net.minecraft.client.renderer.RenderType;
+import net.minecraft.block.material.Material;
+import net.minecraft.block.SoundType;
+import net.minecraft.block.BlockState;
+import net.minecraft.block.Block;
+
+import net.mcreator.varioustextures.itemgroup.MagniumItemGroup;
+import net.mcreator.varioustextures.VariousAdditionsModElements;
+
+import java.util.List;
+import java.util.Collections;
+
+@VariousAdditionsModElements.ModElement.Tag
+public class MagmaLampBlock extends VariousAdditionsModElements.ModElement {
+	@ObjectHolder("various_additions:magma_lamp")
+	public static final Block block = null;
+	public MagmaLampBlock(VariousAdditionsModElements instance) {
+		super(instance, 32);
+	}
+
+	@Override
+	public void initElements() {
+		elements.blocks.add(() -> new CustomBlock());
+		elements.items.add(() -> new BlockItem(block, new Item.Properties().group(MagniumItemGroup.tab)).setRegistryName(block.getRegistryName()));
+	}
+
+	@Override
+	@OnlyIn(Dist.CLIENT)
+	public void clientLoad(FMLClientSetupEvent event) {
+		RenderTypeLookup.setRenderLayer(block, RenderType.getCutout());
+	}
+	public static class CustomBlock extends Block {
+		public CustomBlock() {
+			super(Block.Properties.create(Material.IRON).sound(SoundType.LANTERN).hardnessAndResistance(1f, 10f).setLightLevel(s -> 11).notSolid()
+					.setNeedsPostProcessing((bs, br, bp) -> true).setEmmisiveRendering((bs, br, bp) -> true).setOpaque((bs, br, bp) -> false));
+			setRegistryName("magma_lamp");
+		}
+
+		@Override
+		public boolean propagatesSkylightDown(BlockState state, IBlockReader reader, BlockPos pos) {
+			return true;
+		}
+
+		@Override
+		public List<ItemStack> getDrops(BlockState state, LootContext.Builder builder) {
+			List<ItemStack> dropsOriginal = super.getDrops(state, builder);
+			if (!dropsOriginal.isEmpty())
+				return dropsOriginal;
+			return Collections.singletonList(new ItemStack(this, 1));
+		}
+	}
+}
