dee926476abf804ea522a41588c06a9a4b4c1915
xMiniox <wikto@DESKTOP-EAM10TR>
2021-04-10 15:05:53 +0200

    added star fragment

--- src/main/java/net/mcreator/varioustextures/gui/MagmaInfuserGuiGuiWindow.java
+++ src/main/java/net/mcreator/varioustextures/gui/MagmaInfuserGuiGuiWindow.java
@@ -7,12 +7,16 @@ import net.minecraftforge.api.distmarker.OnlyIn;
 import net.minecraftforge.api.distmarker.Dist;
 
 import net.minecraft.world.World;
+import net.minecraft.util.text.StringTextComponent;
 import net.minecraft.util.text.ITextComponent;
 import net.minecraft.util.ResourceLocation;
 import net.minecraft.entity.player.PlayerInventory;
 import net.minecraft.entity.player.PlayerEntity;
+import net.minecraft.client.gui.widget.button.Button;
 import net.minecraft.client.gui.screen.inventory.ContainerScreen;
 import net.minecraft.client.Minecraft;
+
+import net.mcreator.varioustextures.VariousAdditionsMod;
 
 import com.mojang.blaze3d.matrix.MatrixStack;
 
@@ -33,11 +37,6 @@ public class MagmaInfuserGuiGuiWindow extends ContainerScreen<MagmaInfuserGuiGui
 	}
 
 	@Override
-	public boolean isPauseScreen() {
-		return true;
-	}
-	private static final ResourceLocation texture = new ResourceLocation("various_additions:textures/magma_infuser_gui.png");
-	@Override
 	public void render(MatrixStack ms, int mouseX, int mouseY, float partialTicks) {
 		this.renderBackground(ms);
 		super.render(ms, mouseX, mouseY, partialTicks);
@@ -47,10 +46,6 @@ public class MagmaInfuserGuiGuiWindow extends ContainerScreen<MagmaInfuserGuiGui
 	@Override
 	protected void drawGuiContainerBackgroundLayer(MatrixStack ms, float par1, int par2, int par3) {
 		GL11.glColor4f(1, 1, 1, 1);
-		Minecraft.getInstance().getTextureManager().bindTexture(texture);
-		int k = (this.width - this.xSize) / 2;
-		int l = (this.height - this.ySize) / 2;
-		this.blit(ms, k, l, 0, 0, this.xSize, this.ySize, this.xSize, this.ySize);
 		Minecraft.getInstance().getTextureManager().bindTexture(new ResourceLocation("various_additions:textures/gui6.png"));
 		this.blit(ms, this.guiLeft + 0, this.guiTop + 0, 0, 0, 180, 170, 180, 170);
 	}
@@ -71,6 +66,7 @@ public class MagmaInfuserGuiGuiWindow extends ContainerScreen<MagmaInfuserGuiGui
 
 	@Override
 	protected void drawGuiContainerForegroundLayer(MatrixStack ms, int mouseX, int mouseY) {
+		this.font.drawString(ms, "Magma Infuser", 3, 4, -1);
 	}
 
 	@Override
@@ -83,5 +79,9 @@ public class MagmaInfuserGuiGuiWindow extends ContainerScreen<MagmaInfuserGuiGui
 	public void init(Minecraft minecraft, int width, int height) {
 		super.init(minecraft, width, height);
 		minecraft.keyboardListener.enableRepeatEvents(true);
+		this.addButton(new Button(this.guiLeft + 3, this.guiTop + 33, 55, 20, new StringTextComponent("Infuse"), e -> {
+			VariousAdditionsMod.PACKET_HANDLER.sendToServer(new MagmaInfuserGuiGui.ButtonPressedMessage(0, x, y, z));
+			MagmaInfuserGuiGui.handleButtonAction(entity, 0, x, y, z);
+		}));
 	}
 }
