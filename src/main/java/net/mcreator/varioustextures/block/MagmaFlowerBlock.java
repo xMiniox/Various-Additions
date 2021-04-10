e20feb25629d1583948d4595e7f8379f671effff
xMiniox <wikto@DESKTOP-EAM10TR>
2021-04-10 12:06:30 +0200

    Proper models with holding, magnium ingot recipe

--- src/main/java/net/mcreator/varioustextures/block/MagmaFlowerBlock.java
+++ src/main/java/net/mcreator/varioustextures/block/MagmaFlowerBlock.java
@@ -42,7 +42,7 @@ public class MagmaFlowerBlock extends VariousAdditionsModElements.ModElement {
 	@Override
 	@OnlyIn(Dist.CLIENT)
 	public void clientLoad(FMLClientSetupEvent event) {
-		RenderTypeLookup.setRenderLayer(block, RenderType.getCutout());
+		RenderTypeLookup.setRenderLayer(block, RenderType.getCutoutMipped());
 	}
 	public static class CustomBlock extends Block {
 		public CustomBlock() {
