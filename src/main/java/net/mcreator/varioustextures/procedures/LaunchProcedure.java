ceb24cc3b91d23bd57b003e0a03c3f105ca43e45
xMiniox <wikto@DESKTOP-EAM10TR>
2021-05-01 16:01:24 +0200

    Added Test Launch Button

---
+++ src/main/java/net/mcreator/varioustextures/procedures/LaunchProcedure.java
@@ -0,0 +1,204 @@
+package net.mcreator.varioustextures.procedures;
+
+import net.minecraftforge.registries.ForgeRegistries;
+import net.minecraftforge.eventbus.api.SubscribeEvent;
+import net.minecraftforge.event.TickEvent;
+import net.minecraftforge.common.MinecraftForge;
+
+import net.minecraft.world.server.ServerWorld;
+import net.minecraft.world.World;
+import net.minecraft.world.IWorld;
+import net.minecraft.util.math.BlockPos;
+import net.minecraft.util.SoundCategory;
+import net.minecraft.util.ResourceLocation;
+import net.minecraft.particles.ParticleTypes;
+import net.minecraft.entity.Entity;
+
+import net.mcreator.varioustextures.VariousAdditionsModElements;
+import net.mcreator.varioustextures.VariousAdditionsMod;
+
+import java.util.Map;
+
+@VariousAdditionsModElements.ModElement.Tag
+public class LaunchProcedure extends VariousAdditionsModElements.ModElement {
+	public LaunchProcedure(VariousAdditionsModElements instance) {
+		super(instance, 156);
+	}
+
+	public static void executeProcedure(Map<String, Object> dependencies) {
+		if (dependencies.get("entity") == null) {
+			if (!dependencies.containsKey("entity"))
+				VariousAdditionsMod.LOGGER.warn("Failed to load dependency entity for procedure Launch!");
+			return;
+		}
+		if (dependencies.get("x") == null) {
+			if (!dependencies.containsKey("x"))
+				VariousAdditionsMod.LOGGER.warn("Failed to load dependency x for procedure Launch!");
+			return;
+		}
+		if (dependencies.get("y") == null) {
+			if (!dependencies.containsKey("y"))
+				VariousAdditionsMod.LOGGER.warn("Failed to load dependency y for procedure Launch!");
+			return;
+		}
+		if (dependencies.get("z") == null) {
+			if (!dependencies.containsKey("z"))
+				VariousAdditionsMod.LOGGER.warn("Failed to load dependency z for procedure Launch!");
+			return;
+		}
+		if (dependencies.get("world") == null) {
+			if (!dependencies.containsKey("world"))
+				VariousAdditionsMod.LOGGER.warn("Failed to load dependency world for procedure Launch!");
+			return;
+		}
+		Entity entity = (Entity) dependencies.get("entity");
+		double x = dependencies.get("x") instanceof Integer ? (int) dependencies.get("x") : (double) dependencies.get("x");
+		double y = dependencies.get("y") instanceof Integer ? (int) dependencies.get("y") : (double) dependencies.get("y");
+		double z = dependencies.get("z") instanceof Integer ? (int) dependencies.get("z") : (double) dependencies.get("z");
+		IWorld world = (IWorld) dependencies.get("world");
+		if ((entity.isAlive())) {
+			entity.setNoGravity((true));
+			entity.setMotion(0, 2, 0);
+			if (world instanceof World && !world.isRemote()) {
+				((World) world).playSound(null, new BlockPos((int) x, (int) y, (int) z),
+						(net.minecraft.util.SoundEvent) ForgeRegistries.SOUND_EVENTS.getValue(new ResourceLocation("entity.firework_rocket.launch")),
+						SoundCategory.NEUTRAL, (float) 1, (float) 1);
+			} else {
+				((World) world).playSound(x, y, z,
+						(net.minecraft.util.SoundEvent) ForgeRegistries.SOUND_EVENTS.getValue(new ResourceLocation("entity.firework_rocket.launch")),
+						SoundCategory.NEUTRAL, (float) 1, (float) 1, false);
+			}
+			if (world instanceof ServerWorld) {
+				((ServerWorld) world).spawnParticle(ParticleTypes.FLAME, x, y, z, (int) 100, 3, 3, 3, 0.1);
+			}
+			new Object() {
+				private int ticks = 0;
+				private float waitTicks;
+				private IWorld world;
+				public void start(IWorld world, int waitTicks) {
+					this.waitTicks = waitTicks;
+					MinecraftForge.EVENT_BUS.register(this);
+					this.world = world;
+				}
+
+				@SubscribeEvent
+				public void tick(TickEvent.ServerTickEvent event) {
+					if (event.phase == TickEvent.Phase.END) {
+						this.ticks += 1;
+						if (this.ticks >= this.waitTicks)
+							run();
+					}
+				}
+
+				private void run() {
+					if (world instanceof ServerWorld) {
+						((ServerWorld) world).spawnParticle(ParticleTypes.FLAME, x, y, z, (int) 20, 3, 3, 3, 0.1);
+					}
+					new Object() {
+						private int ticks = 0;
+						private float waitTicks;
+						private IWorld world;
+						public void start(IWorld world, int waitTicks) {
+							this.waitTicks = waitTicks;
+							MinecraftForge.EVENT_BUS.register(this);
+							this.world = world;
+						}
+
+						@SubscribeEvent
+						public void tick(TickEvent.ServerTickEvent event) {
+							if (event.phase == TickEvent.Phase.END) {
+								this.ticks += 1;
+								if (this.ticks >= this.waitTicks)
+									run();
+							}
+						}
+
+						private void run() {
+							if (world instanceof ServerWorld) {
+								((ServerWorld) world).spawnParticle(ParticleTypes.FLAME, x, y, z, (int) 20, 3, 3, 3, 0.1);
+							}
+							new Object() {
+								private int ticks = 0;
+								private float waitTicks;
+								private IWorld world;
+								public void start(IWorld world, int waitTicks) {
+									this.waitTicks = waitTicks;
+									MinecraftForge.EVENT_BUS.register(this);
+									this.world = world;
+								}
+
+								@SubscribeEvent
+								public void tick(TickEvent.ServerTickEvent event) {
+									if (event.phase == TickEvent.Phase.END) {
+										this.ticks += 1;
+										if (this.ticks >= this.waitTicks)
+											run();
+									}
+								}
+
+								private void run() {
+									if (world instanceof ServerWorld) {
+										((ServerWorld) world).spawnParticle(ParticleTypes.FLAME, x, y, z, (int) 20, 3, 3, 3, 0.1);
+									}
+									new Object() {
+										private int ticks = 0;
+										private float waitTicks;
+										private IWorld world;
+										public void start(IWorld world, int waitTicks) {
+											this.waitTicks = waitTicks;
+											MinecraftForge.EVENT_BUS.register(this);
+											this.world = world;
+										}
+
+										@SubscribeEvent
+										public void tick(TickEvent.ServerTickEvent event) {
+											if (event.phase == TickEvent.Phase.END) {
+												this.ticks += 1;
+												if (this.ticks >= this.waitTicks)
+													run();
+											}
+										}
+
+										private void run() {
+											if (world instanceof ServerWorld) {
+												((ServerWorld) world).spawnParticle(ParticleTypes.FLAME, x, y, z, (int) 20, 3, 3, 3, 0.1);
+											}
+											MinecraftForge.EVENT_BUS.unregister(this);
+										}
+									}.start(world, (int) 2);
+									MinecraftForge.EVENT_BUS.unregister(this);
+								}
+							}.start(world, (int) 2);
+							MinecraftForge.EVENT_BUS.unregister(this);
+						}
+					}.start(world, (int) 2);
+					MinecraftForge.EVENT_BUS.unregister(this);
+				}
+			}.start(world, (int) 2);
+			new Object() {
+				private int ticks = 0;
+				private float waitTicks;
+				private IWorld world;
+				public void start(IWorld world, int waitTicks) {
+					this.waitTicks = waitTicks;
+					MinecraftForge.EVENT_BUS.register(this);
+					this.world = world;
+				}
+
+				@SubscribeEvent
+				public void tick(TickEvent.ServerTickEvent event) {
+					if (event.phase == TickEvent.Phase.END) {
+						this.ticks += 1;
+						if (this.ticks >= this.waitTicks)
+							run();
+					}
+				}
+
+				private void run() {
+					entity.setNoGravity((false));
+					MinecraftForge.EVENT_BUS.unregister(this);
+				}
+			}.start(world, (int) 20);
+		}
+	}
+}
