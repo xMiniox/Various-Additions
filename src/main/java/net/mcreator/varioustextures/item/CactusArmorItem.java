ec4ad236a356bf1574b140c6575e453968679e59
xMiniox <wikto@DESKTOP-EAM10TR>
2021-05-12 16:36:48 +0200

    Added cactus Armor

---
+++ src/main/java/net/mcreator/varioustextures/item/CactusArmorItem.java
@@ -0,0 +1,105 @@
+
+package net.mcreator.varioustextures.item;
+
+import net.minecraftforge.registries.ObjectHolder;
+import net.minecraftforge.registries.ForgeRegistries;
+import net.minecraftforge.api.distmarker.OnlyIn;
+import net.minecraftforge.api.distmarker.Dist;
+
+import net.minecraft.util.ResourceLocation;
+import net.minecraft.item.crafting.Ingredient;
+import net.minecraft.item.ItemStack;
+import net.minecraft.item.Item;
+import net.minecraft.item.IArmorMaterial;
+import net.minecraft.item.ArmorItem;
+import net.minecraft.inventory.EquipmentSlotType;
+import net.minecraft.entity.Entity;
+import net.minecraft.block.Blocks;
+
+import net.mcreator.varioustextures.itemgroup.MagniumItemGroup;
+import net.mcreator.varioustextures.VariousAdditionsModElements;
+
+@VariousAdditionsModElements.ModElement.Tag
+public class CactusArmorItem extends VariousAdditionsModElements.ModElement {
+	@ObjectHolder("various_additions:cactus_armor_helmet")
+	public static final Item helmet = null;
+	@ObjectHolder("various_additions:cactus_armor_chestplate")
+	public static final Item body = null;
+	@ObjectHolder("various_additions:cactus_armor_leggings")
+	public static final Item legs = null;
+	@ObjectHolder("various_additions:cactus_armor_boots")
+	public static final Item boots = null;
+	public CactusArmorItem(VariousAdditionsModElements instance) {
+		super(instance, 182);
+	}
+
+	@Override
+	public void initElements() {
+		IArmorMaterial armormaterial = new IArmorMaterial() {
+			@Override
+			public int getDurability(EquipmentSlotType slot) {
+				return new int[]{13, 15, 16, 11}[slot.getIndex()] * 17;
+			}
+
+			@Override
+			public int getDamageReductionAmount(EquipmentSlotType slot) {
+				return new int[]{2, 7, 6, 2}[slot.getIndex()];
+			}
+
+			@Override
+			public int getEnchantability() {
+				return 10;
+			}
+
+			@Override
+			public net.minecraft.util.SoundEvent getSoundEvent() {
+				return (net.minecraft.util.SoundEvent) ForgeRegistries.SOUND_EVENTS.getValue(new ResourceLocation(""));
+			}
+
+			@Override
+			public Ingredient getRepairMaterial() {
+				return Ingredient.fromStacks(new ItemStack(Blocks.CACTUS, (int) (1)));
+			}
+
+			@OnlyIn(Dist.CLIENT)
+			@Override
+			public String getName() {
+				return "cactus_armor";
+			}
+
+			@Override
+			public float getToughness() {
+				return 0f;
+			}
+
+			@Override
+			public float getKnockbackResistance() {
+				return 0f;
+			}
+		};
+		elements.items.add(() -> new ArmorItem(armormaterial, EquipmentSlotType.HEAD, new Item.Properties().group(MagniumItemGroup.tab)) {
+			@Override
+			public String getArmorTexture(ItemStack stack, Entity entity, EquipmentSlotType slot, String type) {
+				return "various_additions:textures/models/armor/cactus__layer_" + (slot == EquipmentSlotType.LEGS ? "2" : "1") + ".png";
+			}
+		}.setRegistryName("cactus_armor_helmet"));
+		elements.items.add(() -> new ArmorItem(armormaterial, EquipmentSlotType.CHEST, new Item.Properties().group(MagniumItemGroup.tab)) {
+			@Override
+			public String getArmorTexture(ItemStack stack, Entity entity, EquipmentSlotType slot, String type) {
+				return "various_additions:textures/models/armor/cactus__layer_" + (slot == EquipmentSlotType.LEGS ? "2" : "1") + ".png";
+			}
+		}.setRegistryName("cactus_armor_chestplate"));
+		elements.items.add(() -> new ArmorItem(armormaterial, EquipmentSlotType.LEGS, new Item.Properties().group(MagniumItemGroup.tab)) {
+			@Override
+			public String getArmorTexture(ItemStack stack, Entity entity, EquipmentSlotType slot, String type) {
+				return "various_additions:textures/models/armor/cactus__layer_" + (slot == EquipmentSlotType.LEGS ? "2" : "1") + ".png";
+			}
+		}.setRegistryName("cactus_armor_leggings"));
+		elements.items.add(() -> new ArmorItem(armormaterial, EquipmentSlotType.FEET, new Item.Properties().group(MagniumItemGroup.tab)) {
+			@Override
+			public String getArmorTexture(ItemStack stack, Entity entity, EquipmentSlotType slot, String type) {
+				return "various_additions:textures/models/armor/cactus__layer_" + (slot == EquipmentSlotType.LEGS ? "2" : "1") + ".png";
+			}
+		}.setRegistryName("cactus_armor_boots"));
+	}
+}
