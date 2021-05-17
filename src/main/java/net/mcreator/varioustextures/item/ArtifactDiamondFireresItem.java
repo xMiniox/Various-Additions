1f41d82772a8d6caa07b205b28337d9488b907ec
xMiniox <wikto@DESKTOP-EAM10TR>
2021-05-17 22:08:51 +0200

    deleted cores, added artifacts

---
+++ src/main/java/net/mcreator/varioustextures/item/ArtifactDiamondFireresItem.java
@@ -0,0 +1,55 @@
+
+package net.mcreator.varioustextures.item;
+
+import net.minecraftforge.registries.ObjectHolder;
+import net.minecraftforge.api.distmarker.OnlyIn;
+import net.minecraftforge.api.distmarker.Dist;
+
+import net.minecraft.item.Rarity;
+import net.minecraft.item.ItemStack;
+import net.minecraft.item.Item;
+import net.minecraft.block.BlockState;
+
+import net.mcreator.varioustextures.itemgroup.MagniumItemGroup;
+import net.mcreator.varioustextures.VariousAdditionsModElements;
+
+@VariousAdditionsModElements.ModElement.Tag
+public class ArtifactDiamondFireresItem extends VariousAdditionsModElements.ModElement {
+	@ObjectHolder("various_additions:artifact_diamond_fireres")
+	public static final Item block = null;
+	public ArtifactDiamondFireresItem(VariousAdditionsModElements instance) {
+		super(instance, 208);
+	}
+
+	@Override
+	public void initElements() {
+		elements.items.add(() -> new ItemCustom());
+	}
+	public static class ItemCustom extends Item {
+		public ItemCustom() {
+			super(new Item.Properties().group(MagniumItemGroup.tab).maxStackSize(64).rarity(Rarity.COMMON));
+			setRegistryName("artifact_diamond_fireres");
+		}
+
+		@Override
+		public int getItemEnchantability() {
+			return 0;
+		}
+
+		@Override
+		public int getUseDuration(ItemStack itemstack) {
+			return 0;
+		}
+
+		@Override
+		public float getDestroySpeed(ItemStack par1ItemStack, BlockState par2Block) {
+			return 1F;
+		}
+
+		@Override
+		@OnlyIn(Dist.CLIENT)
+		public boolean hasEffect(ItemStack itemstack) {
+			return true;
+		}
+	}
+}
---
+++ src/main/java/net/mcreator/varioustextures/procedures/ArtifactFlyProcedureProcedure.java
@@ -0,0 +1,37 @@
+package net.mcreator.varioustextures.procedures;
+
+import net.minecraft.item.ItemStack;
+import net.minecraft.entity.player.PlayerEntity;
+import net.minecraft.entity.LivingEntity;
+import net.minecraft.entity.Entity;
+
+import net.mcreator.varioustextures.item.ArtifactFlyItem;
+import net.mcreator.varioustextures.VariousAdditionsModElements;
+import net.mcreator.varioustextures.VariousAdditionsMod;
+
+import java.util.Map;
+
+@VariousAdditionsModElements.ModElement.Tag
+public class ArtifactFlyProcedureProcedure extends VariousAdditionsModElements.ModElement {
+	public ArtifactFlyProcedureProcedure(VariousAdditionsModElements instance) {
+		super(instance, 197);
+	}
+
+	public static void executeProcedure(Map<String, Object> dependencies) {
+		if (dependencies.get("entity") == null) {
+			if (!dependencies.containsKey("entity"))
+				VariousAdditionsMod.LOGGER.warn("Failed to load dependency entity for procedure ArtifactFlyProcedure!");
+			return;
+		}
+		Entity entity = (Entity) dependencies.get("entity");
+		if (((((entity instanceof LivingEntity) ? ((LivingEntity) entity).getHeldItemMainhand() : ItemStack.EMPTY)
+				.getItem() == new ItemStack(ArtifactFlyItem.block, (int) (1)).getItem())
+				|| (((entity instanceof LivingEntity) ? ((LivingEntity) entity).getHeldItemOffhand() : ItemStack.EMPTY)
+						.getItem() == new ItemStack(ArtifactFlyItem.block, (int) (1)).getItem()))) {
+			if (entity instanceof PlayerEntity) {
+				((PlayerEntity) entity).abilities.isFlying = (true);
+				((PlayerEntity) entity).sendPlayerAbilities();
+			}
+		}
+	}
+}
