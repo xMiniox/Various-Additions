30c3c0a0d61833f387b74e33a1a3d674ba3fdea3
xMiniox <wikto@DESKTOP-EAM10TR>
2021-05-28 17:44:39 +0200

    fixed projectile backwards?

--- models/Modelcustom_model.java
+++ models/Modelcustom_model.java
@@ -20,38 +20,32 @@ public static class Modelcustom_model extends EntityModel<Entity> {
 		cube_r1 = new ModelRenderer(this);
 		cube_r1.setRotationPoint(0.0F, 0.0F, -0.1455F);
 		bb_main.addChild(cube_r1);
-		setRotationAngle(cube_r1, 1.8326F, 0.0F, 0.0F);
+		setRotationAngle(cube_r1, -1.309F, 0.0F, 0.0F);
 		cube_r1.setTextureOffset(0, 0).addBox(-0.5F, -1.2989F, -2.6177F, 1.0F, 1.0F, 3.0F, 0.0F, false);
 
 		cube_r2 = new ModelRenderer(this);
 		cube_r2.setRotationPoint(0.0F, 0.0F, -0.1455F);
 		bb_main.addChild(cube_r2);
-		setRotationAngle(cube_r2, 1.309F, 0.0F, 0.0F);
+		setRotationAngle(cube_r2, -1.8326F, 0.0F, 0.0F);
 		cube_r2.setTextureOffset(0, 0).addBox(-0.5F, 0.2989F, -2.6177F, 1.0F, 1.0F, 3.0F, 0.0F, false);
 
 		cube_r3 = new ModelRenderer(this);
 		cube_r3.setRotationPoint(0.0F, 0.0F, -0.1455F);
 		bb_main.addChild(cube_r3);
-		setRotationAngle(cube_r3, 1.5708F, 0.0F, -0.2618F);
+		setRotationAngle(cube_r3, -1.5708F, 0.0F, 0.2618F);
 		cube_r3.setTextureOffset(0, 0).addBox(-1.2989F, -0.5F, -2.6177F, 1.0F, 1.0F, 3.0F, 0.0F, false);
 
 		cube_r4 = new ModelRenderer(this);
 		cube_r4.setRotationPoint(0.0F, 0.0F, -0.1455F);
 		bb_main.addChild(cube_r4);
-		setRotationAngle(cube_r4, 1.5708F, 0.0F, 0.2618F);
+		setRotationAngle(cube_r4, -1.5708F, 0.0F, -0.2618F);
 		cube_r4.setTextureOffset(0, 0).addBox(0.2989F, -0.5F, -2.6177F, 1.0F, 1.0F, 3.0F, 0.0F, false);
 
 		cube_r5 = new ModelRenderer(this);
 		cube_r5.setRotationPoint(0.0F, 0.0F, -0.1455F);
 		bb_main.addChild(cube_r5);
-		setRotationAngle(cube_r5, 1.5708F, 0.0F, 0.0F);
+		setRotationAngle(cube_r5, -1.5708F, 0.0F, 0.0F);
 		cube_r5.setTextureOffset(0, 0).addBox(-0.5F, -0.5F, 0.1455F, 1.0F, 1.0F, 10.0F, 0.0F, false);
-	}
-
-	@Override
-	public void setRotationAngles(Entity entity, float limbSwing, float limbSwingAmount, float ageInTicks,
-			float netHeadYaw, float headPitch) {
-		// previously the render function, render code was moved to a method below
 	}
 
 	@Override
@@ -64,5 +58,9 @@ public static class Modelcustom_model extends EntityModel<Entity> {
 		modelRenderer.rotateAngleX = x;
 		modelRenderer.rotateAngleY = y;
 		modelRenderer.rotateAngleZ = z;
+	}
+
+	public void setRotationAngles(float f, float f1, float f2, float f3, float f4, float f5, Entity e) {
+		super.setRotationAngles(f, f1, f2, f3, f4, f5, e);
 	}
 }
