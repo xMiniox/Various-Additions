e8e5cf9555bbdb581e14731eabb4fd980e2e0025
xMiniox <wikto@DESKTOP-EAM10TR>
2021-06-02 08:55:52 +0200

    Added Tabelyst, deleted amogus

---
+++ src/main/java/net/mcreator/varioustextures/block/CobbledTabelystBlock.java
@@ -0,0 +1,48 @@
+
+package net.mcreator.varioustextures.block;
+
+import net.minecraftforge.registries.ObjectHolder;
+
+import net.minecraft.loot.LootContext;
+import net.minecraft.item.ItemStack;
+import net.minecraft.item.Item;
+import net.minecraft.item.BlockItem;
+import net.minecraft.block.material.Material;
+import net.minecraft.block.SoundType;
+import net.minecraft.block.BlockState;
+import net.minecraft.block.Block;
+
+import net.mcreator.varioustextures.itemgroup.MagniumItemGroup;
+import net.mcreator.varioustextures.VariousAdditionsModElements;
+
+import java.util.List;
+import java.util.Collections;
+
+@VariousAdditionsModElements.ModElement.Tag
+public class CobbledTabelystBlock extends VariousAdditionsModElements.ModElement {
+	@ObjectHolder("various_additions:cobbled_tabelyst")
+	public static final Block block = null;
+	public CobbledTabelystBlock(VariousAdditionsModElements instance) {
+		super(instance, 260);
+	}
+
+	@Override
+	public void initElements() {
+		elements.blocks.add(() -> new CustomBlock());
+		elements.items.add(() -> new BlockItem(block, new Item.Properties().group(MagniumItemGroup.tab)).setRegistryName(block.getRegistryName()));
+	}
+	public static class CustomBlock extends Block {
+		public CustomBlock() {
+			super(Block.Properties.create(Material.ROCK).sound(SoundType.STONE).hardnessAndResistance(1f, 10f).setLightLevel(s -> 0));
+			setRegistryName("cobbled_tabelyst");
+		}
+
+		@Override
+		public List<ItemStack> getDrops(BlockState state, LootContext.Builder builder) {
+			List<ItemStack> dropsOriginal = super.getDrops(state, builder);
+			if (!dropsOriginal.isEmpty())
+				return dropsOriginal;
+			return Collections.singletonList(new ItemStack(this, 1));
+		}
+	}
+}
